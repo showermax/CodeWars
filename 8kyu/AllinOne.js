5dc1e8db1b221e6db1842686aa3f3c36a77d4eaa
showermax <sevenhauz@gmail.com>
2023-02-04 01:21:28 +0300

    Update AllinOne.js

--- 8kyu/AllinOne.js
+++ 8kyu/AllinOne.js
@@ -31,3 +31,12 @@ function disemvowel(str) {
     return str.replace(/[aeiou]]/gi, '');
 console.log(str);
 }
+
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+function paperwork(n, m) {
+    return (n < 0 || m < 0) ? 0 : m * n;
+  }
