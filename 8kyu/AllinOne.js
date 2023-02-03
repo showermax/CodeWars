363ca3a3e82771959f13c20fa1bc8a1a3d0b437b
showermax <sevenhauz@gmail.com>
2023-02-04 01:43:16 +0300

    Update AllinOne.js

--- 8kyu/AllinOne.js
+++ 8kyu/AllinOne.js
@@ -40,3 +40,19 @@ console.log(str);
 function paperwork(n, m) {
     return (n < 0 || m < 0) ? 0 : m * n;
   }
+
+//   You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+//   #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string. 
+
+function getMiddle(s) {
+    if (s.length % 2 == 0) {
+      return s[s.length / 2 -1] + s[s.length / 2];
+    }
+    return s[(s.length - 1) / 2];
+  }
