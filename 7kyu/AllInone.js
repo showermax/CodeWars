affdd261f8ee1871573bed67b84560e52b837e0d
showermax <sevenhauz@gmail.com>
2023-02-06 02:44:13 +0300

    Update AllInone.js

--- 7kyu/AllInone.js
+++ 7kyu/AllInone.js
@@ -18,3 +18,12 @@ function findShort(s){
       } else {return false};
     }
   }
+  //Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+  function isTriangle(a,b,c)
+{
+  if (a + b > c && a + c > b && b + c > a){
+    return true;
+  }
+   return false;
+}
