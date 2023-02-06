7f6ff46a6d23378333182ec60337e3093a87ea1b
showermax <sevenhauz@gmail.com>
2023-02-06 12:20:13 +0300

    added 1

--- 7kyu/AllInone.js
+++ 7kyu/AllInone.js
@@ -27,3 +27,20 @@ function findShort(s){
   }
    return false;
 }
+function basicOp(operation, value1, value2)
+{
+  switch (operation) {
+      case "+":
+      return value1 + value2;
+      break;
+      case "-":
+      return value1 - value2;
+      break;
+      case "*":
+      return value1 * value2;
+      break;
+      case "/":
+      return value1 / value2;
+      break;
+  }
+}
