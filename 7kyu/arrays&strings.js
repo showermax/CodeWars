c6537787e5330174ef4e091d34f68adb27f0ff3c
showermax <sevenhauz@gmail.com>
2023-02-08 16:56:44 +0300

    Vowel Count

--- 7kyu/arrays&strings.js
+++ 7kyu/arrays&strings.js
@@ -4,6 +4,8 @@ function descendingOrder(n){
 }
 
 
+//Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
 function sumOfMinimums(arr) {
   let sum = 0;
   let i=0;
@@ -18,3 +20,29 @@ function sumOfMinimums(arr) {
   return sum;
 }
   console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));
+
+//   Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  let i=0;
+for (let j = 0; j < str.length; j++) {
+      switch (str[j]) {
+          case "a" : i++;
+          break;
+          case "e" : i++;
+          break;
+          case "i" : i++;
+          break;
+          case "o" : i++;
+          break;
+          case "u" : i++;
+          break;
+      }
+    }
+  return i;
+}
+
