0be8d1c115a54e21d1f5cac5dd3b21675f824e2a
showermax <sevenhauz@gmail.com>
2023-03-11 21:42:26 +0300

    queue

--- 6kyu/Math.js
+++ 6kyu/Math.js
@@ -4,11 +4,11 @@
 function findNb(m) {
     let k = 0;
     let n = 0;
-do {
-    n++;
-    k += Math.pow(n,3);
-} while (k < m);
-return (k == m) ? n : -1;
+    do {
+        n++;
+        k += Math.pow(n, 3);
+    } while (k < m);
+    return (k == m) ? n : -1;
 }
 
 //console.log(findNb(100));
@@ -24,20 +24,20 @@ return (k == m) ? n : -1;
 // If it is the case we will return k, if not return -1.
 
 // Note: n and p will always be given as strictly positive integers.
-function digPow(n, p){
-   let arr = [];
-   let sum = 0;
-   let narr = n;
-    while ( narr > 0) {
-       arr.unshift(Math.round(narr % 10));
-        narr = narr/10 - narr%10/10;       
+function digPow(n, p) {
+    let arr = [];
+    let sum = 0;
+    let narr = n;
+    while (narr > 0) {
+        arr.unshift(Math.round(narr % 10));
+        narr = narr / 10 - narr % 10 / 10;
     }
-    sum = arr.reduce((s,e,i) => s + e**(p+i),0);
-    return  ((sum % n) === 0) ? sum/n : -1;
+    sum = arr.reduce((s, e, i) => s + e ** (p + i), 0);
+    return ((sum % n) === 0) ? sum / n : -1;
 }
-  //console.log(digPow(46288, 3));
+//console.log(digPow(46288, 3));
 
-  function Xbonacci(signature,n){
+function Xbonacci(signature, n) {
     let sum = 0;
     let l = signature.length;
     for (let i = l; i < n; i++) {
@@ -47,46 +47,75 @@ function digPow(n, p){
         }
         signature.push(sum);
     }
-    return signature.slice(0,n);
-  }
-  //console.log(Xbonacci([0,0,0,0,1],10));
+    return signature.slice(0, n);
+}
+//console.log(Xbonacci([0,0,0,0,1],10));
 
-  function persistence(num) {
+function persistence(num) {
     let arr = [];
     let i = 0;
     if (num < 10) return 0;
     loop1:
     while (num > 9) {
-    while ( num > 0) {
-        arr.unshift(Math.round(num % 10));
-        num = num/10 - num%10/10;       
-     }
-     i++;
-    if (arr.reduce((p, e) => p*e) < 10) {
-        return i;
-    } else {
-        num = arr.reduce((p, e) => p*e);
-        arr=[];
-        continue loop1;
+        while (num > 0) {
+            arr.unshift(Math.round(num % 10));
+            num = num / 10 - num % 10 / 10;
+        }
+        i++;
+        if (arr.reduce((p, e) => p * e) < 10) {
+            return i;
+        } else {
+            num = arr.reduce((p, e) => p * e);
+            arr = [];
+            continue loop1;
+        }
     }
 }
-}
- //console.log(persistence(4));
+//console.log(persistence(4));
 
- function findUniq(arr) {  
-    
-    let x =  arr.find(e => e == 0);
+function findUniq(arr) {
+
+    let x = arr.find(e => e == 0);
     return x;
-   
+
     let s = 0;
-    let i = 0;     
-        while (s==0) {
-           s = arr[i+1] - arr[i];
-           i++;
+    let i = 0;
+    while (s == 0) {
+        s = arr[i + 1] - arr[i];
+        i++;
+    }
+    if (i < arr.length - 1) {
+        return (arr[i + 1] == arr[i - 1]) ? arr[i] : arr[i - 1];
+    }
+    else return arr[i];
+}
+//console.log(findUniq([0, 0, 1]));
+
+function queueTime(customers, n) {
+    let min = customers[0];
+    let max = customers[0];
+    let i=0;
+    let k=0;
+    if (customers.length === 0) return 0
+    if (n === 1) return customers.reduce((a, b) => a + b)
+    while ((n + i) < customers.length) {
+        for (let j = 0; j < n; j++) {
+            if (customers[j] < min) {
+                min = customers[j];
+                k = j;
+            }
         }
-        if (i<arr.length-1) {
-        return (arr[i+1] == arr[i-1]) ? arr[i] : arr[i-1];
-        } 
-        else return arr[i];
-  }
-  console.log(findUniq([ 0, 0, 1 ]));
+        customers[k] = customers[k] + customers[n + i]
+        i++;
+        min = customers[0];
+        k=0;
+        console.log(customers)
+    }
+    for (let i = 0; i < n; i++) {
+        if (customers[i]>max) max = customers[i];
+    }
+    return max
+}
+console.log(queueTime([5,3,4],    1));
+
+
